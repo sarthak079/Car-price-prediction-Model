3cb31fa79d5b20688be321ac75b675c105ce8d3b
Sarthak Singhal <sarthaksinghal3107@gmail.com>
2026-03-10 15:19:00 +0530

    changes in schema

--- car-price-api/schema.py
+++ car-price-api/schema.py
@@ -15,12 +15,12 @@ class TransmissionType(str, Enum):
 
 class CarFeatures(BaseModel):
     Car_name: str = Field(..., alias="Car_Name")
-    Year: int = Field(...) # Changed to int to match your JSON data
+    Year: int = Field(...)  # No alias needed as it matches 'Year'
     Present_price: float = Field(..., alias="Present_Price")
     Kms_driven: int = Field(..., alias="Kms_Driven")
     Fuel_type: str = Field(..., alias="Fuel_Type")
     Seller_type: str = Field(..., alias="Seller_Type")
-    Transmission: str = Field(...) 
+    Transmission: str = Field(...) # No alias needed as it matches 'Transmission'
     Owner: int = Field(...)
 
 class PredictionResponse(BaseModel):
